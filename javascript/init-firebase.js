4e4586d93d8842738c65adbea94978b074494a1f
elephanteatbanana <hyeon01030103@gmail.com>
2019-12-04 13:06:23 -0800

    init-firebase.js added and coded

---
+++ javascript/init-firebase.js
@@ -0,0 +1,13 @@
+console.log("Firebase has been initiated!");
+const firebaseConfig = {
+    apiKey: "AIzaSyDmUvyRSPByioirqCbaGpV8M5NTQ3qfyEs",
+    authDomain: "vticket-e9391.firebaseapp.com",
+    databaseURL: "https://vticket-e9391.firebaseio.com",
+    projectId: "vticket-e9391",
+    storageBucket: "vticket-e9391.appspot.com",
+    messagingSenderId: "1024714277075",
+    appId: "1:1024714277075:web:cd3eafec7e6eeb6f"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
