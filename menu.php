808db379a5e16fb98114bf2363c8bad37a8789d2
elephanteatbanana <hyeon01030103@gmail.com>
2019-12-04 23:36:30 -0800

    menu.php : html : sports,concerts,arts and locals section structured

--- menu.php
+++ menu.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Vancouver's Best Ticket Booking : Vticket</title>
 
-     <!-- Firebase setting -->
-     <script src="https://www.gstatic.com/firebasejs/7.1.0/firebase-app.js"></script>
+    <!-- Firebase setting -->
+    <script src="https://www.gstatic.com/firebasejs/7.1.0/firebase-app.js"></script>
     <script src="https://www.gstatic.com/firebasejs/7.1.0/firebase-auth.js"></script>
     <script src="https://www.gstatic.com/firebasejs/7.1.0/firebase-firestore.js"></script>
 
@@ -22,6 +22,7 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <!--  header  -->
     <div id="menu-header-logo">
         <a href="menu.php"><img src="category/images/logo.png" alt="logo"></a>
 
@@ -34,6 +35,7 @@
 
     </div>
 
+    <!--  navigation  -->
     <div class="menu-category">
         <div class="menu-category-name">
             <a href="category/concert.php">Concerts</a>
@@ -48,6 +50,47 @@
             <a href="category/local.php">Local</a>
         </div>
     </div>
+    <!--  main  -->
+    <div class="menu-main">
+        <div class="menu-cat-tab">
+            <div class="menu-cat-tab-header">
+                <h3>Concerts</h3>
+                <a href="category/concert.php"><h4>See all</h4></a>
+            </div>
+            <div class="menu-cat-tab-main">
+
+            </div>
+        </div>
+        <div class="menu-cat-tab">
+            <div class="menu-cat-tab-header">
+                <h3>Sports</h3>
+                <a href="category/sport.php"><h4>See all</h4></a>
+            </div>
+            <div class="menu-cat-tab-main">
+                
+            </div>
+        </div>
+        <div class="menu-cat-tab">
+            <div class="menu-cat-tab-header">
+                <h3>Arts</h3>
+                <a href="category/art.php"><h4>See all</h4></a>
+            </div>
+            <div class="menu-cat-tab-main">
+                
+            </div>
+        </div>
+        <div class="menu-cat-tab">
+            <div class="menu-cat-tab-header">
+                <h3>Locals</h3>
+                <a href="category/local.php"><h4>See all</h4></a>
+            </div>
+            <div class="menu-cat-tab-main">
+                
+            </div>
+        </div>
+    </div>
+
+    <!--  footer  -->
     <footer>
         <div class="footer-content">
             <p>By continuing past this page, you agree to our Terms of Use.</p>
