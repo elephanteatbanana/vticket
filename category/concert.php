3248831d80ea32b8174928d2bec9a7b568636132
elephanteatbanana <hyeon01030103@gmail.com>
2019-12-05 13:54:44 -0800

    ajax : live data load feature added, sort data by name and date feature added

--- category/concert.php
+++ category/concert.php
@@ -39,7 +39,7 @@ require 'includes/vticket.db.php';
         <ul id="listTab" class="menu-category">
             <li class="menu-category-name concertsTab active" rel="concertsPanel"> <a href="concert.php">Concerts</a></li>
             <li class="menu-category-name sportsTab" rel="sportsPanel"> <a href="sport.php">Sports</a></li>
-            <li class="menu-category-name artsTab" rel="artsPanel"> <a href="art.php">Arts</a></li>          
+            <li class="menu-category-name artsTab" rel="artsPanel"> <a href="art.php">Arts</a></li>
             <li class="menu-category-name localTab" rel="localPanel"> <a href="local.php">Local</a></li>
         </ul>
         <!--Content container -->
@@ -47,26 +47,26 @@ require 'includes/vticket.db.php';
             <h3>Concerts</h3>
             <div>
                 <!--Sort by -->
-                <select name="selectCat" class="cat-sort-select">
+                <select name="selectCat" class="cat-sort-select" onchange="sortBy(this.value)">
                     <option value="">Default</option>
                     <option value="namea">By Name(A-Z)</option>
                     <option value="namez">By Name(Z-A)</option>
                     <option value="new">Newest First</option>
-                    <option value="old">Oldest First</option>   
+                    <option value="old">Oldest First</option>
                 </select>
             </div>
             <div id="cat-sort-result">
                 <?php
                 /* fetch only 4 pieces of data from 'concerts' */
-                $query= 'SELECT * FROM `concerts` ORDER BY id LIMIT 4';
+                $query = 'SELECT * FROM `concerts` ORDER BY id LIMIT 4';
                 if ($is_query_run = mysqli_query($conn, $query)) {
                     echo '<ul class="category-list-ul">';
                     while ($row = mysqli_fetch_assoc($is_query_run)) {
                         echo '<div class="category-event">';
-                        $date=date_create($row['date']);
+                        $date = date_create($row['date']);
                         $mdy = date_format($date, "M d, D, Y");
-                        $hm = date_format($date, "h:m")." PM";
-                        echo '<img src="'.$row["Image_dir"].'" alt=""><li><a href="detail.php?cat=con&id='.$row["id"].'"><p class="cat-title">'.$row["title"].'</p><p class="cat-location">'. $row["location"].'</p><p class="cat-mdy">'. $mdy.'</p><p class="cat-hm">'.$hm.'</p></a></li>';
+                        $hm = date_format($date, "h:m") . " PM";
+                        echo '<img src="' . $row["Image_dir"] . '" alt=""><li><a href="detail.php?cat=con&id=' . $row["id"] . '"><p class="cat-title">' . $row["title"] . '</p><p class="cat-location">' . $row["location"] . '</p><p class="cat-mdy">' . $mdy . '</p><p class="cat-hm">' . $hm . '</p></a></li>';
                         echo '</div>';
                     }
                     echo '</ul>';
@@ -77,16 +77,56 @@ require 'includes/vticket.db.php';
                 }
                 ?>
             </div>
-            <button>Load More</button>
+            <button onclick="getData()">Load More</button>
         </div>
     </div>
     <footer>
         <div class="footer-content">
             <p>By continuing past this page, you agree to our Terms of Use.</p>
             <p>Privacy Policy | 2019 Vticket. All rights reserved.</p>
-        </div>  
-    </footer> 
+        </div>
+    </footer>
 </body>
+<script>
+    var from = 0;
+    var to = 4;
+    /* sort by function - connected to "sort by" above */
+    function sortBy(str){
+        $("#cat-sort-result").empty();
+        from = 0;
+        to = 4;
+        if(window.XMLHttpRequest){
+            xmlhttp = new XMLHttpRequest ();
+        }else{
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function(){
+            if(this.readyState==4 && this.status==200){
+                document.getElementById("cat-sort-result").innerHTML=this.responseText;
+            }
+        }
+        xmlhttp.open("GET", "list.php?q=c" + str + "&from=" + from + "&to=" + to, true);
+        xmlhttp.send();
+    }
+    /* Load more data - connected to "load more" button above */
+    function getData(){
+        to = 4 + to ;
+        $("#cat-sort-result").empty();
+        str = $( ".cat-sort-select option:selected" ).val();
+        if(window.XMLHttpRequest){
+            xmlhttp = new XMLHttpRequest ();
+        }else{
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function(){
+            if(this.readyState==4 && this.status==200){
+                document.getElementById("cat-sort-result").innerHTML=this.responseText;
+            }
+        }
+        xmlhttp.open("GET", "list.php?q=c" + str + "&from=" + from + "&to=" + to, true);
+        xmlhttp.send();
+    }
+</script>
 <script src="../javascript/init-firebase.js"></script>
 <script src="../javascript/concert.js"></script>
 </html>
