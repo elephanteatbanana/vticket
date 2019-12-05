dfc2f4b13c0514a74876010e85fd0b28d540ae31
elephanteatbanana <hyeon01030103@gmail.com>
2019-12-05 13:35:59 -0800

    html : page structured , libraries added

---
+++ category/concert.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vancouver's Best Ticket Booking : Concerts</title>
+
+    <!-- Firebase setting -->
+    <script  src="https://www.gstatic.com/firebasejs/7.1.0/firebase-app.js"></script>
+    <script  src="https://www.gstatic.com/firebasejs/7.1.0/firebase-auth.js"></script>
+    <script  src="https://www.gstatic.com/firebasejs/7.1.0/firebase-firestore.js"></script>
+
+    <!--Ajax -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+    <!--Font-->
+    <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+    <!--  kit's code  -->
+    <script src="https://kit.fontawesome.com/25199e113c.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div id="menu-header-logo">
+        <a href="../menu.php"><img src="images/logo.png" alt="logo"></a>
+        <div class="menu-header-logo-icons">
+            <a class="header-logo-icon" href="../myPage.php"><i class="fas fa-user"></i></a>
+            <div class="header-logo-icon" id="signoutBt">
+                <i class="fas fa-sign-out-alt"></i>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
