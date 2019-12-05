310cdabb35464e4683dbba52c7b117c7ca80c4fd
elephanteatbanana <hyeon01030103@gmail.com>
2019-12-04 23:48:35 -0800

    add new variables, add sign-out function, add a status-change function

---
+++ javascript/menu.js
@@ -0,0 +1,25 @@
+//Vars
+
+const signoutButton = document.getElementById('signoutBt');
+
+//Sign out
+
+signoutButton.addEventListener('click', e => {
+    console.log("Signed out");
+    firebase.auth().signOut();
+    window.location.replace("home.html");
+})
+
+
+//status change
+
+firbase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log("User is in");
+    } else {
+        console.log("User is not in");
+        console.log("Signed out");
+        firebase.auth().signOut();
+        window.location.replace("home.html");
+    }
+})
