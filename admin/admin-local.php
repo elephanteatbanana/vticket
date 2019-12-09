68747683dda29850251cbea2eb333bfe615def4f
elephanteatbanana <hyeon01030103@gmail.com>
2019-12-09 12:40:15 -0800

    typo corrected

--- admin/admin-local.php
+++ admin/admin-local.php
@@ -8,7 +8,7 @@ $result = "";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Vancouver's Best Ticket Booking : Admin Arts</title>
+    <title>Vancouver's Best Ticket Booking : Admin Locals</title>
     <!-- Firebase setting -->
     <script  src="https://www.gstatic.com/firebasejs/7.1.0/firebase-app.js"></script>
     <script  src="https://www.gstatic.com/firebasejs/7.1.0/firebase-auth.js"></script>
